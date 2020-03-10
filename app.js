3c4ac4ebe14f3175fea3b002636d56b524298a93
Oliver Weber <oliver@mondough.io>
2020-03-10 13:44:42 +0100

    before restucturing routes

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use((req, _, next) => {
+  console.log(req.method, req.url);
+
+  next();
+});
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/cute-cat", (req, res) => {
+  res.send({ title: "look at this cutie" });
+});
+
+app.post("/cute-cat", (req, res) => {
+  res.send({ title: "posted cute cat" });
+});
 
 app.get("/", function(req, res, next) {
   res.send({ title: "Express" });
