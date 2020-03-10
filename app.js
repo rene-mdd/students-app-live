c297534be43dc045174ef6a4f8224755dff5b0a8
Oliver Weber <oliver@mondough.io>
2020-03-10 14:32:19 +0100

    register routes

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-var app = express();
+const app = express();
+
+// - GET (all, individual)
+app.get("/api/students", (req, res) => {
+  res.send({});
+});
+// - PUT (individual)
+app.put("/api/students", (req, res) => {
+  res.send({});
+});
+// - DELETE (individual)
+app.delete("/api/students", (req, res) => {
+  res.send({});
+});
+// - POST (individual)
+app.post("/api/students", (req, res) => {
+  res.send({});
+});
 
 module.exports = app;
