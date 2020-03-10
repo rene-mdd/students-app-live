b8b1fec53e273d74d5b31a45f30176c04697ae51
Oliver Weber <oliver@mondough.io>
2020-03-10 15:22:02 +0100

    students server implementation (draft)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const students = [
+let students = [
   {
     name: "Rupert",
     lastname: "Jalili",
@@ -14,21 +14,59 @@ const students = [
   }
 ];
 
+app.use(express.json());
+
 // - GET (all, individual)
 app.get("/api/students", (req, res) => {
   res.status(200).json(students);
 });
+
+app.get("/api/students/:name", (req, res) => {
+  const student = students.find(
+    ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
+  );
+
+  if (student) {
+    return res.status(200).json(student);
+  }
+
+  res.status(404).json({ error: "Student not found" });
+});
+
 // - PUT (individual)
-app.put("/api/students", (req, res) => {
-  res.send({});
+app.put("/api/students/:name", (req, res) => {
+  if (req.params.name && req.body) {
+    students = students.map((student) => {
+      if (student.name.toLowerCase() === req.params.name.toLowerCase()) {
+        Object.assign(student, req.body);
+      }
+
+      return student;
+    });
+  }
+  res.send(students);
 });
 // - DELETE (individual)
-app.delete("/api/students", (req, res) => {
-  res.send({});
+app.delete("/api/students/:name", (req, res) => {
+  if (req.params.name) {
+    students = students.filter(
+      ({ name }) => name.toLowerCase() !== req.params.name.toLowerCase()
+    );
+  }
+
+  res.send(students);
 });
 // - POST (individual)
 app.post("/api/students", (req, res) => {
-  res.send({});
+  if (req.body) {
+    students.push(req.body);
+    return res.send({
+      status: "success",
+      message: `student with name: ${req.body.name} added`
+    });
+  }
+
+  res.send("NO!");
 });
 
 module.exports = app;
