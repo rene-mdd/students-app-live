5303ad444e9f93514f96dc025c02e1fce34d85a4
Oliver Weber <oliver@mondough.io>
2020-03-10 14:13:56 +0100

    prepare students app

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 
-const cuteCatRouter = require("./routes/cuteCatRoutes");
-
 var app = express();
-
-app.use((req, _, next) => {
-  console.log(req.method, req.url);
-
-  next();
-});
-app.use("/public", express.static(path.join(__dirname, "public")));
-
-app.use("/cute-cats", cuteCatRouter);
-
-app.get("/", function(req, res) {
-  res.send({ title: "Express" });
-});
-
-app.use("*", (req, res) => {
-  res.status(404).send("Page not found");
-});
 
 module.exports = app;
